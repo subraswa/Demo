a6f63c3c2dc7d804711202425bbcc6b9cec49531
Swain <subrat-kumar.a.swain@capgemini.com>
2021-11-30 12:52:33 +0530

    updated

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JenkinDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
